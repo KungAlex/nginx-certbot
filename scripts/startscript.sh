34c8704c5c705ee9ea6dae4b17f58ba13241f427
kungalex <alexander.kleinschmidt@smail.fh-koeln.de>
2017-07-20 02:17:47 +0200

    fixes

--- scripts/startscript.sh
+++ scripts/startscript.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-#openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-#echo $CERTBOT_DOMAIN_PREFIX.$CERTBOT_DOMAIN
 
-## TODO use certbot nginx config file and restart with nginx -t -c file
+## Nginx logs to stdout
+tail -n 0 -f /var/log/nginx/*.log &tail -n 0 -f /var/log/nginx/*.log &
+
+## Setup for Certbot
+# TODO run certbot only on the first startup
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 ln -s /etc/nginx/sites/defaults-letsencrypt /etc/nginx/sites-enabled/defaults-letsencrypt
 rm /etc/nginx/sites-enabled/default
 
 nginx -t
 service nginx restart &
 
-## Nginx logs to stdout
-tail -n 0 -f /var/log/nginx/*.log &
+certbot certonly --webroot --webroot-path=/var/www/html -d $CERTBOT_DOMAIN_PREFIX.$CERTBOT_DOMAIN --email $CERTBOT_EMAIL --agree-tos
 
-sleep infinity
+rm /etc/nginx/sites-enabled/defaults-letsencrypt
+ln -s /etc/nginx/sites/$CERTBOT_DOMAIN_PREFIX.$CERTBOT_DOMAIN /etc/nginx/sites-enabled/$CERTBOT_DOMAIN_PREFIX.$CERTBOT_DOMAIN
 
-## TODO run certbot on the first startup
-#certbot certonly --webroot --webroot-path=/var/www/html -d $CERTBOT_DOMAIN_PREFIX.$CERTBOT_DOMAIN --email $CERTBOT_EMAIL
-
-
-## TODO change nginx config file and restart with nginx -t -c file
-#rm /etc/nginx/sites-enabled/defaults-letsencrypt
-#cp /etc/nginx/scripts/auth.kungfooo /etc/nginx/sites-enabled/auth.kungfooo
-#service nginx restart &
+service nginx restart &
 
 ## TODO set Cron-Job for Certbot on $CERTBOT_RENEWAL_DAYS
-# sleep infinity
+sleep infinity
